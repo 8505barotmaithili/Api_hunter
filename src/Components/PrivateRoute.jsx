33a0edc9a853b615d81bcfac151a9a9ad71e21b8
8505barotmaithili <8505barotmaithili@gmail.com>
2025-04-27 22:45:44 +0530

    first commit

---
+++ src/Components/PrivateRoute.jsx
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { AuthContext } from "../Context/AuthContext";
+import { Navigate } from "react-router-dom";
+
+function PrivateRoute({ children }) {
+  const { authstate } = useContext(AuthContext);
+
+  if (!authstate.isauth) {
+    return <Navigate to="/login" />;
+  } else {
+    return children;
+  }
+}
+
+export default PrivateRoute;
